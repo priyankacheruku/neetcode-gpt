e97b240ec46b3a2605c442c725b29304d055d42f
Cheruku Priyanka <priyankacheruku2016@gmail.com>
2026-07-05 14:43:18 +0530

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -9,10 +9,4 @@ class Solution:
         # Hint: subtract max(z) for numerical stability before computing exp
         # return np.round(your_answer, 4)
         m = np.max(z)
-        r =[]
-        for i in z:
-            r.append(np.exp(i - m))
-        de = sum(r)
-        for i in range(len(r)):
-            r[i] = np.round(r[i]/de,4)
-        return np.array(r)
+        return np.round(np.exp(z-m)/(np.sum(np.exp(z-m))),4)
